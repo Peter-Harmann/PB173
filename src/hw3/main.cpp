d83f284671d11a798579ee24b0b417726bd42fbb
Peter Harmann <peto.harmann@gmail.com>
2018-12-01 19:45:04 +0100

    hw3: fixed (mostly redone)

--- src/hw3/main.cpp
+++ src/hw3/main.cpp
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <bitset>
 #include <array>
+#include <memory>
 
 using namespace std;
 using namespace std::literals::chrono_literals;
@@ -61,118 +62,135 @@ public:
 	}
 };
 
-class Trie {
-	static Trie * const LeafTrie;
-	std::array<Trie*, 16> other;
+template <int level>
+class Node {
+	std::array<unique_ptr<Node<level-1>>, 16> children;
+	
 public:
+	Node() {}
+	Node(const Node<level> & other) { for(size_t i = 0; i < 16; ++i) children[i].reset(!other.children[i] ? nullptr : new Node<level-1>(*other.children[i])); }
+	Node(Node<level> && other) { for(size_t i = 0; i < 16; ++i) { children[i] = other.children[i]; other.children[i] = nullptr; } }
+	~Node() {}
+	
 	bool empty() const {
-		for(size_t i = 0; i < 16; ++i) {
-			if(other[i] != nullptr) return false;
-		}
+		for(size_t i = 0; i < 16; ++i) if(children[i] && !children[i]->empty()) return false;
 		return true;
 	}
-
-	Trie() {
-		for(size_t i = 0; i<16; ++i) other[i] = nullptr;
-	}
-	Trie(const Trie & o) {
-		for(size_t i=0; i<16; ++i) {
-			if(o.other[i]) {
-				if (o.other[i] == LeafTrie) this->other[i] = LeafTrie;
-				else this->other[i] = new Trie(*o.other[i]);
-			}
-			else this->other[i] = nullptr;
+	void insert(uint16_t num) {
+		if(!children[num & 0xF]) {
+			children[num & 0xF] = make_unique<Node<level-1>>();
+			children[num & 0xF]->insert(num >> 4);
 		}
+		else children[num & 0xF]->insert(num >> 4);
 	}
-	Trie(Trie && o) {
-		for (size_t i = 0; i < 16; ++i) {
-			other[i] = o.other[i];
-			o.other[i] = nullptr;
-		}
-	}
-	~Trie() {
-		for (size_t i = 0; i < 16; ++i) {
-			if (other[i] != LeafTrie) {
-				delete other[i];
-			}
-			other[i] = nullptr;
-		}
-	}
-	Trie & operator=(const Trie &) = delete;
-	Trie & operator=(Trie &&) = delete;
-
-	void insert(uint16_t num, unsigned int level = 4) {
-		Trie* & o = other[num & 0xF];
-		if(level == 1) o = LeafTrie;
-		else if(o == nullptr) {
-			Trie * nt = new Trie;
-			nt->insert(num >> 4, level-1);
-			o = nt;
-		}
-		else o->insert(num >> 4, level-1);
-	}
-	void erase(uint16_t num, unsigned int level = 4) { 
-		Trie* & o = other[num & 0xF];
-		if(level == 1) o = nullptr;
-		else if(o != nullptr) {
-			o->erase(num >> 4, level-1);
-			if(o->empty()) {
-				delete o;
-				o = nullptr;
-			}
-		}	
-	}
-	
-	Trie* union_op(const Trie & o, unsigned int level = 4) {
-		Trie* res = new Trie;
-		if(level == 1) {
-			for(size_t i=0; i<16; ++i) {
-				if(o.other[i] || this->other[i]) res->other[i] = LeafTrie;
+	void erase(uint16_t num) {
+		if(children[num & 0xF]) {
+			children[num & 0xF]->erase(num >> 4);
+			if(children[num & 0xF]->empty()) {
+				children[num & 0xF].reset();
 			}
 		}
-		else {
-			for(size_t i=0; i<16; ++i) {
-				if(this->other[i]) {
-					if(o.other[i]) res->other[i] = this->other[i]->union_op(*o.other[i], level-1);
-					else res->other[i] = new Trie(*this->other[i]);
-				}
-				else if(o.other[i]) res->other[i] = new Trie(*o.other[i]);
+	}
+	unique_ptr<Node<level>> operator|(const Node<level> & o) {
+		unique_ptr<Node<level>> res = make_unique<Node<level>>();
+		for(size_t i = 0; i < 16; ++i) {
+			if(children[i]) {
+				if(o.children[i]) res->children[i] = *children[i] | *o.children[i];
+				else res->children[i] = make_unique<Node<level-1>>(*children[i]);
+			}
+			else {
+				if(o.children[i]) res->children[i] = make_unique<Node<level-1>>(*o.children[i]);
 			}
 		}
 		return res;
 	}
-	
-	Trie* intersection_op(const Trie & o, unsigned int level = 4){
-		Trie* res = new Trie;
-		if(level == 1) {
-			for(size_t i=0; i<16; ++i) {
-				if(o.other[i] && this->other[i]) res->other[i] = LeafTrie;
-			}
-		}
-		else {
-			for(size_t i=0; i<16; ++i) {
-				if(this->other[i] && o.other[i]) res->other[i] = this->other[i]->intersection_op(*o.other[i],level-1);
-			}
-		}
-		if(res->empty() && level < 4) {
-			delete res;
-			return nullptr;
+	unique_ptr<Node<level>> operator&(const Node<level> & o) {
+		unique_ptr<Node<level>> res = make_unique<Node<level>>();
+		for(size_t i = 0; i < 16; ++i) {
+			if(children[i] && o.children[i]) res->children[i] = *children[i] & *o.children[i];
 		}
 		return res;
 	}
 };
 
-Trie* const Trie::LeafTrie = (Trie *)0xFFFFFFFF;
+template<>
+class Node<1> {
+	std::bitset<16> number;
+	
+public:
+	Node() {}
+	Node(const Node<1> & other) : number(other.number) {}
+	Node(Node<1> && other) : number(std::move(other.number)) {}
+	~Node() {}
+	
+	bool empty() const { return number.any(); }
+	void insert(uint16_t num) { number[num & 0xF] = true; }
+	void erase(uint16_t num) { number[num & 0xF] = false; }
+	unique_ptr<Node<1>> operator|(const Node<1> & o) {
+		unique_ptr<Node<1>> res = make_unique<Node<1>>();
+		res->number = number | o.number;
+		return res;
+	}
+	unique_ptr<Node<1>> operator&(const Node<1> & o) {
+		unique_ptr<Node<1>> res = make_unique<Node<1>>();
+		res->number = number & o.number;
+		return res;
+	}
+};
+
+class Trie {
+	unique_ptr<Node<4>> root;
+public:
+	bool empty() const { return root->empty(); }
+
+	Trie() : root(new Node<4>()) {}
+	Trie(unique_ptr<Node<4>> && n) : root(std::move(n)) {}
+	Trie(const Trie & o) : root(make_unique<Node<4>>(*o.root)) {}
+	Trie(Trie && o) : root(o.root.release()) {}
+	~Trie() {}
+	Trie & operator=(const Trie &) = delete;
+	Trie & operator=(Trie &&) = delete;
+
+	void insert(uint16_t num) { root->insert(num); }
+	void erase(uint16_t num) { root->erase(num); }
+	
+	unique_ptr<Trie> union_op(const Trie & o) { return make_unique<Trie>(*root | *o.root); }
+	unique_ptr<Trie> intersection_op(const Trie & o) { return make_unique<Trie>(*root & *o.root); }
+};
+
+const size_t single_iterations_bitvector_insert = 150000;
+const size_t single_iterations_trie_insert = 75000;
+const size_t single_iterations_bitvector_erase = 150000;
+const size_t single_iterations_trie_erase = 120000;
+const size_t single_iterations_bitvector_union = 3000;
+const size_t single_iterations_trie_union = 5000;
+const size_t single_iterations_bitvector_intersect = 3000;
+const size_t single_iterations_trie_intersect = 60000;
+
+size_t trie_union_increment(unsigned long long iter) {
+	if(iter <= 32) 		return 32;
+	if(iter <= 64) 		return 64;
+	if(iter <= 128)		return 108;
+	if(iter <= 256)		return 187;
+	if(iter <= 512)		return 281;
+	if(iter <= 1024)	return 432;
+	if(iter <= 2048)	return 617;
+	if(iter <= 4086)	return 752;
+	if(iter <= 8192)	return 752;
+	if(iter <= 16384)	return 752;
+	return 752;
+}
 
 void bench1(Benchmark & bench, unsigned long long iterations) {
 	Bitvector test;
 	auto dis = rng.getDistribution(0, 65535);
 	
 	bench.start();
-	for(size_t i=0; i<iterations; ++i) {
+	for(size_t i=0; i < single_iterations_bitvector_insert; ++i) {
 		test.insert(dis());
 	}
 	bench.stop();
+	bench.set_repeats(single_iterations_bitvector_insert);
 }
 
 void bench2(Benchmark & bench, unsigned long long iterations) {
@@ -180,40 +198,43 @@ void bench2(Benchmark & bench, unsigned long long iterations) {
 	auto dis = rng.getDistribution(0, 65535);
 	
 	bench.start();
-	for(size_t i=0; i<iterations; ++i) {
+	for(size_t i=0; i < single_iterations_trie_insert; ++i) {
 		test.insert(dis());
 	}
 	bench.stop();
+	bench.set_repeats(single_iterations_trie_insert);
 }
 
 void bench3(Benchmark & bench, unsigned long long iterations) {
 	Bitvector test;
 	auto dis = rng.getDistribution(0, 65535);
 	
-	for(size_t i=0; i<40000; ++i) {
+	for(size_t i=0; i<iterations; ++i) {
 		test.insert(dis());
 	}
 	
 	bench.start();
-	for(size_t i=0; i<iterations; ++i) {
+	for(size_t i=0; i < single_iterations_bitvector_erase; ++i) {
 		test.erase(dis());
 	}
 	bench.stop();
+	bench.set_repeats(single_iterations_bitvector_erase);
 }
 
 void bench4(Benchmark & bench, unsigned long long iterations) {
 	Trie test;
 	auto dis = rng.getDistribution(0, 65535);
 	
-	for(size_t i=0; i<40000; ++i) {
+	for(size_t i=0; i<iterations; ++i) {
 		test.insert(dis());
 	}
 	
 	bench.start();
-	for(size_t i=0; i<iterations; ++i) {
+	for(size_t i=0; i < single_iterations_trie_erase; ++i) {
 		test.erase(dis());
 	}
 	bench.stop();
+	bench.set_repeats(single_iterations_trie_erase);
 }
 
 void bench5(Benchmark & bench, unsigned long long iterations) {
@@ -221,18 +242,19 @@ void bench5(Benchmark & bench, unsigned long long iterations) {
 	Bitvector test2;
 	auto dis = rng.getDistribution(0, 65535);
 	
-	for(size_t i=0; i<30000; ++i) {
+	for(size_t i=0; i<iterations; ++i) {
 		test.insert(dis());
 		test2.insert(dis());
 	}
 	
 	bench.start();
-	for(size_t i=0; i<iterations; ++i) {
+	for(size_t i=0; i < single_iterations_bitvector_union; ++i) {
 		Bitvector * r = test.union_op(test2);
 		r->empty();
 		delete r;
 	}
 	bench.stop();
+	bench.set_repeats(single_iterations_bitvector_union);
 }
 
 void bench6(Benchmark & bench, unsigned long long iterations) {
@@ -240,18 +262,21 @@ void bench6(Benchmark & bench, unsigned long long iterations) {
 	Trie test2;
 	auto dis = rng.getDistribution(0, 65535);
 	
-	for(size_t i=0; i<30000; ++i) {
+	for(size_t i=0; i<iterations; ++i) {
 		test.insert(dis());
 		test2.insert(dis());
 	}
 	
+	size_t increment = trie_union_increment(iterations);
+	size_t iter_count = 0;
 	bench.start();
-	for(size_t i=0; i<iterations; ++i) {
-		Trie * r = test.union_op(test2);
+	for(size_t i=0; i < single_iterations_trie_union; i += increment) {
+		unique_ptr<Trie> r = test.union_op(test2);
 		r->empty();
-		delete r;
+		++iter_count;
 	}
 	bench.stop();
+	bench.set_repeats(iter_count);
 }
 
 void bench7(Benchmark & bench, unsigned long long iterations) {
@@ -259,18 +284,19 @@ void bench7(Benchmark & bench, unsigned long long iterations) {
 	Bitvector test2;
 	auto dis = rng.getDistribution(0, 65535);
 	
-	for(size_t i=0; i<30000; ++i) {
+	for(size_t i=0; i<iterations; ++i) {
 		test.insert(dis());
 		test2.insert(dis());
 	}
 	
 	bench.start();
-	for(size_t i=0; i<iterations; ++i) {
+	for(size_t i=0; i < single_iterations_bitvector_intersect; ++i) {
 		Bitvector * r = test.intersection_op(test2);
 		r->empty();
 		delete r;
 	}
 	bench.stop();
+	bench.set_repeats(single_iterations_bitvector_intersect);
 }
 
 void bench8(Benchmark & bench, unsigned long long iterations) {
@@ -278,18 +304,20 @@ void bench8(Benchmark & bench, unsigned long long iterations) {
 	Trie test2;
 	auto dis = rng.getDistribution(0, 65535);
 	
-	for(size_t i=0; i<30000; ++i) {
+	for(size_t i=0; i<iterations; ++i) {
 		test.insert(dis());
 		test2.insert(dis());
 	}
 	
+	size_t iter_count = 0;
 	bench.start();
-	for(size_t i=0; i<iterations; ++i) {
-		Trie * r = test.intersection_op(test2);
+	for(size_t i = 0; i < single_iterations_trie_intersect; i += iterations) {
+		unique_ptr<Trie> r = test.intersection_op(test2);
 		r->empty();
-		delete r;
+		++iter_count;
 	}
 	bench.stop();
+	bench.set_repeats(iter_count);
 }
 
 int main(int argc, char ** argv) {
@@ -328,25 +356,26 @@ int main(int argc, char ** argv) {
 		start_time = std::chrono::high_resolution_clock().now();
 		
 		// Basic sizes
-		bench_1.run(time, 32000, precision);
-		bench_2.run(time, 32000, precision);
-		bench_3.run(time, 32000, precision);
-		bench_4.run(time, 32000, precision);
-		bench_5.run(time, 32000, precision);
-		bench_6.run(time, 32000, precision);
-		bench_7.run(time, 32000, precision);
-		bench_8.run(time, 32000, precision);
+		bench_1.run(time, 0, precision);
+		bench_2.run(time, 0, precision);
+		bench_3.run(time, bin_sequence(32,10), precision);
+		bench_4.run(time, bin_sequence(32,10), precision);
+		bench_5.run(time, bin_sequence(32,10), precision);
+		bench_6.run(time, bin_sequence(32,10), precision);
+		bench_7.run(time, bin_sequence(32,10), precision);
+		bench_8.run(time, bin_sequence(32,10), precision);
 		
 		mid_time = std::chrono::high_resolution_clock().now();
 		
-		std::cout << bench_1.getStats() << std::flush;
-		std::cout << bench_2.getStats() << std::flush;
-		std::cout << bench_3.getStats() << std::flush;
-		std::cout << bench_4.getStats() << std::flush;
-		std::cout << bench_5.getStats() << std::flush;
-		std::cout << bench_6.getStats() << std::flush;
-		std::cout << bench_7.getStats() << std::flush;
-		std::cout << bench_8.getStats() << std::endl;
+		bool extra_stats = false;
+		std::cout << bench_1.getStats(extra_stats) << std::flush;
+		std::cout << bench_2.getStats(extra_stats) << std::flush;
+		std::cout << bench_3.getStats(extra_stats) << std::flush;
+		std::cout << bench_4.getStats(extra_stats) << std::flush;
+		std::cout << bench_5.getStats(extra_stats) << std::flush;
+		std::cout << bench_6.getStats(extra_stats) << std::flush;
+		std::cout << bench_7.getStats(extra_stats) << std::flush;
+		std::cout << bench_8.getStats(extra_stats) << std::endl;
 		
 		end_time = std::chrono::high_resolution_clock().now();
 		
@@ -363,5 +392,5 @@ int main(int argc, char ** argv) {
 	catch (const std::exception & e) {
 		std::cout << e.what() << std::endl;
 	}
-    return 0;
+	return 0;
 }
